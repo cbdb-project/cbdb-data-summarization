fbef0f174c14befc17741f71d1eb1bfaf29bfc6e
sudoghut <sudoghut@users.noreply.github.com>
2026-09-14 08:03:15 +0000

    Update generated files

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ const chart1Data = [
 { label: "Liao", value: 341 },
 { label: "Jin", value: 799 },
 { label: "Yuan", value: 25311 },
-{ label: "Ming", value: 225532 },
-{ label: "Qing", value: 237826 },
+{ label: "Ming", value: 225541 },
+{ label: "Qing", value: 237869 },
 { label: "Minguo", value: 4686 }
 ];// Chart 1 Data 
 const chart2Data = [
-{ label: "Number of Persons 人數", value: 661659 },
-{ label: "Social Associations 社會關係", value: 190036 },
-{ label: "Biographical Addresses 地址", value: 461320 },
-{ label: "Alternate Names 別名", value: 208771 },
-{ label: "Kin Relationships 親屬關係", value: 562265 },
-{ label: "Entry into Office 入仕", value: 264905 },
-{ label: "Office Postings 任官", value: 591256 },
-{ label: "Social Distinction 社會區分", value: 73450 },
+{ label: "Number of Persons 人數", value: 661712 },
+{ label: "Social Associations 社會關係", value: 190044 },
+{ label: "Biographical Addresses 地址", value: 461372 },
+{ label: "Alternate Names 別名", value: 208790 },
+{ label: "Kin Relationships 親屬關係", value: 562367 },
+{ label: "Entry into Office 入仕", value: 264925 },
+{ label: "Office Postings 任官", value: 591369 },
+{ label: "Social Distinction 社會區分", value: 73469 },
 { label: "Texts 著作", value: 62377 }
 ];// Chart 2 Data
 const chart4Data = [
@@ -47,15 +47,19 @@ const chart4Data = [
 { label: "Yuan", gender: "女", value: 899 },
 { label: "Yuan", gender: "男", value: 24337 },
 { label: "Ming", gender: "女", value: 36839 },
-{ label: "Ming", gender: "男", value: 184501 },
+{ label: "Ming", gender: "男", value: 184510 },
 { label: "Ming", gender: "nan", value: 4192 },
-{ label: "Qing", gender: "男", value: 214011 },
+{ label: "Qing", gender: "男", value: 214043 },
 { label: "Qing", gender: "nan", value: 14396 },
-{ label: "Qing", gender: "女", value: 9419 },
+{ label: "Qing", gender: "女", value: 9430 },
 { label: "Minguo", gender: "女", value: 22 },
 { label: "Minguo", gender: "nan", value: 459 },
 { label: "Minguo", gender: "男", value: 4205 }
 ];//Chart 4 Data
+
+
+
+
 
 
 
